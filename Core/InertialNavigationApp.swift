2dcd175cfea562432305e4e9230b1df10fa8ede3
Michael Shaffer <75963180+Michael-Shaffer@users.noreply.github.com>
2025-04-02 12:08:14 -0400

    Initial commit

---
+++ Core/InertialNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InertialNavigationApp.swift
+//  InertialNavigation
+//
+//  Created by Michael Shaffer on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct InertialNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SensorsView()
+        }
+    }
+}
